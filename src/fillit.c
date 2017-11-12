49c872d9c6da9b458a00b6e6ab260fdd9f26d6e2
alucas- <lucas.abel@yandex.com>
2017-11-12 11:48:56 +0100

    fillit cli args handling

--- src/fillit.c
+++ src/fillit.c
@@ -6,23 +6,31 @@
 /*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/11 17:01:09 by alucas-           #+#    #+#             */
-/*   Updated: 2017/11/11 18:03:45 by alucas-          ###   ########.fr       */
+/*   Updated: 2017/11/12 11:44:53 by alucas-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-t_i32	main(void)
+t_i32	ft_exit(t_i32 code, t_car *msg)
+{
+	if (msg)
+		ft_putendl(msg);
+	else
+		ft_putchar('\n');
+	return (code);
+}
+
+t_i32	main(t_i32 ac, t_car **av)
 {
 	t_ctx	ctx;
 	t_usz	i;
 
+	if (ac != 2)
+		return (ft_exit(EXIT_FAILURE, "usage: fillit source_file"));
 	FT_INIT(&ctx, t_ctx);
 	if (fillit_parse_file(&ctx, "map01"))
-	{
-		ft_putstr("error");
-		return (EXIT_FAILURE);
-	}
+		return (ft_exit(EXIT_FAILURE, "error"));
 	i = 0;
 	while (i < ctx.nelms)
 		ft_putnbr(fillit_validate(ctx.elms[i++]));

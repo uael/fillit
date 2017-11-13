0522e488d83a7dad426c93eeca73365cdf8a83cc
alucas- <lucas.abel@yandex.com>
2017-11-13 10:25:04 +0100

    much faster tetrimino formating

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/11 17:01:09 by alucas-           #+#    #+#             */
-/*   Updated: 2017/11/12 16:28:05 by alucas-          ###   ########.fr       */
+/*   Updated: 2017/11/13 10:19:45 by alucas-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,28 @@ static t_u08	fillit_validate(t_u16 form)
 	return ((t_u08)(c < 3));
 }
 
-static t_u08	fillit_formbeg(t_u16 form)
+static void		fillit_format(t_tetr *tetr)
 {
 	t_i32 x;
 	t_i32 y;
-	t_i32 i;
 
-	i = -1;
-	while (++i < 16)
-	{
-		x = (i % 4);
-		y = ((i - x) / 4);
-		while (x < 4)
-			if (BIT_AT(form, y, x++, 4))
-				while (y < 4)
-					if (BIT_AT(form, y++, (i % 4), 4))
-						return ((t_u08)i);
-	}
-	return (0);
+	if (tetr->form & 0xF)
+		x = 0;
+	else if (tetr->form & 0xF0)
+		x = 1;
+	else if (tetr->form & 0xF00)
+		x = 2;
+	else
+		x = 3;
+	if (tetr->form & 0x1111)
+		y = 0;
+	else if (tetr->form & 0x2222)
+		y = 1;
+	else if (tetr->form & 0x4444)
+		y = 2;
+	else
+		y = 3;
+	tetr->form >>= (x * 4) + y;
 }
 
 static t_u08	fillit_parse_tetr(t_tetr *tetr, t_car **str)
@@ -79,7 +83,7 @@ static t_u08	fillit_parse_tetr(t_tetr *tetr, t_car **str)
 		return (1);
 	while (**str == '\n')
 		++*str;
-	tetr->form >>= fillit_formbeg(tetr->form);
+	fillit_format(tetr);
 	return (0);
 }
 
